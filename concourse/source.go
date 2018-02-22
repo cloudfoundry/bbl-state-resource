d55744e60736516148d1108f356ec1d4e530636d
Rowan Jacobs <rojacobs@pivotal.io>
2018-02-22 15:42:06 -0800

    always omit empty
    Signed-off-by: Connor Braa <connor.braa@gmail.com>

--- concourse/source.go
+++ concourse/source.go
@@ -1,12 +1,12 @@
 package concourse
 
 type Source struct {
-	Name string `json:"name,omitempty" yaml:"name" structs:"name"`
-	IAAS string `json:"iaas,omitempty" yaml:"iaas" structs:"iaas"`
+	Name string `json:"name,omitempty" yaml:"name" structs:"name,omitempty"`
+	IAAS string `json:"iaas,omitempty" yaml:"iaas" structs:"iaas,omitempty"`
 
 	LBType   string `json:"lb-type,omitempty" yaml:"lb-type" structs:"lb-type,omitempty"`
 	LBDomain string `json:"lb-domain,omitempty" yaml:"lb-domain" structs:"lb-domain,omitempty"`
 
-	GCPServiceAccountKey string `json:"gcp-service-account-key,omitempty" yaml:"gcp-service-account-key" structs:"gcp-service-account-key"`
-	GCPRegion            string `json:"gcp-region,omitempty" yaml:"gcp-region" structs:"gcp-region"`
+	GCPServiceAccountKey string `json:"gcp-service-account-key,omitempty" yaml:"gcp-service-account-key" structs:"gcp-service-account-key,omitempty"`
+	GCPRegion            string `json:"gcp-region,omitempty" yaml:"gcp-region" structs:"gcp-region,omitempty"`
 }
