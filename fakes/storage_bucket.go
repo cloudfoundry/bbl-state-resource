752f90aace99e17eca104e12f8030c7e85ce4280
Rowan Jacobs <rojacobs@pivotal.io>
2018-04-02 15:14:09 -0700

    fake bucket has Delete()
    Signed-off-by: Connor Braa <connor.braa@gmail.com>

--- fakes/storage_bucket.go
+++ fakes/storage_bucket.go
@@ -9,8 +9,17 @@ type Bucket struct {
 			Error   error
 		}
 	}
+	DeleteCall struct {
+		Returns struct {
+			Error error
+		}
+	}
 }
 
 func (b *Bucket) GetAllObjects() ([]storage.Object, error) {
 	return b.ObjectsCall.Returns.Objects, b.ObjectsCall.Returns.Error
+}
+
+func (b *Bucket) Delete() error {
+	return b.DeleteCall.Returns.Error
 }
