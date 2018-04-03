86c4bf420e61ab4b2e852214d8dd587692ca3282
Genevieve LEsperance <glesperance@pivotal.io>
2018-04-03 14:58:08 -0700

    trim spaces for names loaded from files
    Signed-off-by: Connor Braa <connor.braa@gmail.com>

--- outrunner/name.go
+++ outrunner/name.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
+	"strings"
 
 	randomdata "github.com/Pallinder/go-randomdata"
 	"github.com/cloudfoundry/bbl-state-resource/concourse"
@@ -25,7 +26,7 @@ func Name(sourcesDir string, params concourse.OutParams) (string, error) {
 		if err != nil {
 			return "", fmt.Errorf("Failure reading name file: %s", err)
 		}
-		return string(name), nil
+		return strings.TrimSpace(string(name)), nil
 	}
 
 	return fmt.Sprintf("%s-%s", randomdata.Adjective(), randomdata.Noun()), nil
